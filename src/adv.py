5ab4d4d0bc3e35bead64d52e62ea4a8752d9453f
nikibhatt <nkibhatt@users.noreply.github.com>
2020-04-09 10:29:15 -0700

    assignment part-1

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -6,24 +8,24 @@ room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty \
+passages run north and east."),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", "A steep cliff appears before you, falling \
+into the darkness. Ahead to the north, a light flickers in \
+the distance, but there is no way across the chasm."),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west \
+to north. The smell of gold permeates the air."),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", "You've found the long-lost treasure \
+chamber! Sadly, it has already been completely emptied by \
+earlier adventurers. The only exit is to the south.")
 }
 
 
 # Link rooms together
-
+#
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -39,8 +41,32 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player('outside')
+
 # Write a loop that:
 #
+
+play_status = 'p'
+
+while play_status != 'q':
+    print(f"You are in {player.current_room}")
+    print("Print n:s:e:w to move q to quit")
+    direction = input("Enter your choice: ")
+    if direction not in ('s','e','w','q', 'n'):
+        print("Invalid input")
+    elif direction == 'q':
+        break
+    if direction == 's' and room[player.current_room].s_to != '':
+        player.current_room = room[player.current_room].s_to.name
+    elif direction == 'n' and room[player.current_room].n_to != '':
+        player.current_room = room[player.current_room].n_to.name
+    elif direction == 'e' and room[player.current_room].e_to != '':
+        player.current_room = room[player.current_room].e_to.name
+    elif direction == 'w' and room[player.current_room].w_to != '':
+        player.current_room = room[player.current_room].w_to.name
+    else:
+        print("Move not allowed")
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+ #Implement a class to hold room information. This should have name and
+ #description attributes.
+
+class Room():
+     def __init__(self, name, description, north='', south='', east='', west=''):
+        self.name = name
+        self.description = description
+        self.n_to = north
+        self.s_to = south
+        self.e_to = east
+        self.w_to = west
